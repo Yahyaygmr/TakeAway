c82317bd6a14a22017f79a65950b1f65cb1e9bfb
Yahyaygmr <yahyaygmr@gmail.com>
2024-08-06 15:37:56 +0300

    end of the lesson

---
+++ TakeAway/IdentityServer/TakeAway.IdentityServer/Controllers/LoginsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TakeAway.IdentityServer.Dtos;
+using TakeAway.IdentityServer.Models;
+using TakeAway.IdentityServer.Tools;
+
+namespace TakeAway.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginsController : ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public LoginsController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+        [HttpPost]
+        public async Task<IActionResult> UserLogin(LoginDto dto)
+        {
+
+            var result = await _signInManager.PasswordSignInAsync(dto.UserName, dto.Password, false, false);
+            var user = await _userManager.FindByNameAsync(dto.UserName);
+            if (result.Succeeded)
+            {
+                GetCheckAppUserViewModel model = new GetCheckAppUserViewModel();
+                model.UserName = dto.UserName;
+                model.Role = "Rol 1";
+                model.Id = user.Id;
+                var token = JwtTokenGenerator.GenerateToken(model);
+
+                return Ok(token);
+            }
+            else
+            {
+                return Ok("Kullanıcı adı veya şifre hatalı.");
+            }
+        }
+    }
+}
